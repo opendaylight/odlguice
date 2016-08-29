opendaylight/infrautils

@Inject convenience helper (org.opendaylight.infrautils.inject)
Introduces the Lifecycle interrface with the LifecycleSupport and
SingletonWithLifecycle convenience base classes, with a
ModuleSetupRuntimeException, to standardize the use of @Inject (in
@Singleton) in the ODL code base for projects using the
blueprint-maven-plugin to generate wiring XML from these Java
annotations instead of hand writing it.

Change-Id: I88313940370e6cbef30bd471b2cda8238be5beb8
Signed-off-by: Michael Vorburger <vorburger@redhat.com>

inject/inject/src/main/java/org/opendaylight/infrautils/inject/Lifecycle.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.inject;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * Something which {@link #start()}s and {@link #stop()}s.
+ *
+ * <p>Annotated so that Dependency Injection Frameworks (whichever) automatically call these methods during wiring.
+ *
+ * @see LifecycleSupport
+ * @see SingletonWithLifecycle
+ *
+ * @author Michael Vorburger
+ */
+public interface Lifecycle {
+
+    @PostConstruct
+    void start() throws ModuleSetupRuntimeException;
+
+    @PreDestroy
+    void stop() throws ModuleSetupRuntimeException;
+
+    boolean isRunning();
+
+}

opendaylight/odlguice

inject/src/main/java/org/opendaylight/infrautils/inject/Lifecycle.java
@@ -1,0 +1,33 @@
+/*
+ * Copyright (c) 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.inject;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * Something which can be {@link #init()}-ialized and {@link #destroy()}-d.
+ *
+ * <p>Annotated so that Dependency Injection Frameworks (whichever) automatically call these methods during wiring.
+ *
+ * @see AbstractLifecycle
+ * @see SingletonWithLifecycle
+ *
+ * @author Michael Vorburger
+ */
+public interface Lifecycle {
+
+    @PostConstruct
+    void init() throws ModuleSetupRuntimeException;
+
+    @PreDestroy
+    void destroy() throws ModuleSetupRuntimeException;
+
+    boolean isRunning();
+
+}